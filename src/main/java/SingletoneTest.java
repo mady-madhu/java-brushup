11e3ebfa2f66da3f801805b4bb559f0fed8f5278
unknown <chittemmadhusudhanareddy@gmail.com>
2024-08-08 14:10:47 -0600

    singletone pattern

---
+++ src/main/java/SingletoneTest.java
@@ -0,0 +1,29 @@
+import org.example.single.SystemObj;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class SingletoneTest {
+    public static void main(String[] args) {
+        SystemObj systemObj1 = SystemObj.getSystemObj();
+        System.out.println(systemObj1);
+
+        SystemObj systemObj2 = SystemObj.getSystemObj();
+        System.out.println(systemObj2);
+
+
+        //Under Environment variables like env1=env1;env2=env2 separated by ;
+        Map<String,String> envMap = systemObj1.getEnv();
+        System.out.println(envMap.get("env1"));
+        System.out.println(envMap.get("env2"));
+
+
+        //Under JVM Options like -Denv1=env1 -Denv2=env2 separated by space
+        Properties p = systemObj1.getProperties();
+        System.out.println(p.getProperty("vm1"));
+        System.out.println(p.getProperty("vm2"));
+
+
+    }
+}
---
+++ src/main/java/org/example/single/SystemObj.java
@@ -0,0 +1,28 @@
+package org.example.single;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class SystemObj {
+
+    private static SystemObj systemObj= null;
+
+    private HashMap props;
+
+    public static SystemObj getSystemObj() {
+        if(systemObj==null){
+            systemObj = new SystemObj();
+        }
+        return systemObj;
+    }
+
+    public Properties getProperties(){
+        return System.getProperties();
+    }
+
+
+    public Map<String, String> getEnv(){
+        return System.getenv();
+    }
+}
